c07661d34e56ee83c32e0e721bcc2526098dc82e
BoYang <datapunch2021@gmail.com>
2022-04-25 23:47:42 -0700

    Rename database to rdsdatabase

--- pkg/topologyimpl/rdsdatabase/topology.go
+++ pkg/topologyimpl/rdsdatabase/topology.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package database
+package rdsdatabase
 
 import (
 	"fmt"
@@ -26,7 +26,7 @@ import (
 const (
 	DefaultUserName = "user1"
 
-	KindDatabaseTopology = "Database"
+	KindDatabaseTopology = "RdsDatabase"
 
 	FieldMaskValue = "***"
 
