3269b9374eeafb3bf2a5459be8d765540c86c89c
BoYang <datapunch2021@gmail.com>
2022-05-15 17:35:01 -0700

    Update spark operator image

--- pkg/topologyimpl/sparkoneks/topology.go
+++ pkg/topologyimpl/sparkoneks/topology.go
@@ -27,7 +27,7 @@ import (
 const (
 	DefaultApiUserName                       = "user1"
 	DefaultOperatorImageRepository           = "ghcr.io/datapunchorg/spark-on-k8s-operator"
-	DefaultSparkOperatorImageTag             = "sha-461928d"
+	DefaultSparkOperatorImageTag             = "sha-f846a30"
 	DefaultSparkOperatorNamespace            = "spark-operator-01"
 	DefaultSparkOperatorHelmInstallName      = "spark-operator-01"
 	DefaultSparkApplicationNamespace         = "spark-01"
