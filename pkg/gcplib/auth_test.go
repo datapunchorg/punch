50f5707520b0dbc8cc2c7b5751da42c170c7727c
BoYang <14280154+hiboyang@users.noreply.github.com>
2023-01-01 00:19:37 -0800

    Disable TestGetCurrentUserAccessToken

--- pkg/gcplib/auth_test.go
+++ pkg/gcplib/auth_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestGetCurrentUserAccessToken(t *testing.T) {
+func DisableTest_TestGetCurrentUserAccessToken(t *testing.T) {
 	token, err := GetCurrentUserAccessToken()
 	assert.Nil(t, err)
 	assert.NotEmpty(t, token)
